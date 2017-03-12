b60f59801b6f967afddc944bc50d3fa7bfd0b059
Chris Lunsford <chris@cmlccie.com>
2017-03-12 00:07:24 -0500

    Vendorize Dependencies
    Vendorize all packages not included in the default Amazon Lambda AMI.

--- smartsheet-dynamodb-sync.py
+++ smartsheet-dynamodb-sync.py
@@ -5,8 +5,16 @@
 
 import logging
 import os
+import sys
 
 import boto3
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+sys.path.append(vendor_dir)
+
+# Import vendorized packages
 import smartsheet as smartsheet_sdk
 
 
